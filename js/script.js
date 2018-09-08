103265ec85f3d11f9b221662114588b8c90e6bba
Lucas Padua <lucas.p2222@gmail.ciom>
2018-09-08 10:39:04 +1000

    navbar fix responsive

--- js/script.js
+++ js/script.js
@@ -15,28 +15,16 @@ $(document).ready(function() {
 
       });
 
-      $('.writers-accordion').click(function(event) {
-
-        if(event.target.id.split('-')[0] === 'button') {
-           $('#book-1').attr('src', 'img/writers/' + event.target.id.split('-')[1] + '-book1.jpg');
-           $('#book-2').attr('src', 'img/writers/' + event.target.id.split('-')[1] + '-book2.jpg');
-        }
-
+      $('.nav-button').click(function() {
+        $('.nav-button').toggleClass('change');
       });
-});
 
-
-$(document).ready(function() {
-  $('.nav-button').click(function() {
-    $('.nav-button').toggleClass('change');
-  });
-
-  $(window).scroll(function() {
-    let position = $(this).scrollTop();
-    if(position >= 200) {
-      $('.nav-menu').addClass('custom-navbar');
-    } else {
-      $('.nav-menu').removeClass('custom-navbar');
-    }
-  });
+      $(window).scroll(function() {
+        let position = $(this).scrollTop();
+        if(position >= 200) {
+          $('.nav-menu').addClass('custom-navbar');
+        } else {
+          $('.nav-menu').removeClass('custom-navbar');
+        }
+      });
 });
